7e19b6bbe424e9b3fe3cf786a843fc499eaf90dc
Murat Gül <murat.coddev@gmail.com>
2025-05-02 15:59:12 +0300

    update

--- src/main/java/com/murat/library/LibraryApp.java
+++ src/main/java/com/murat/library/LibraryApp.java
@@ -11,13 +11,13 @@ public class LibraryApp {
     LibraryManager manager = new LibraryManager();
     HorrorBook b1 = new HorrorBook("doul","murat",56,"Horor",
     LocalDate.of(2021,2,4),LocalDate.now(),"Killer",6);
-        HorrorBook b2 = new HorrorBook("doul","murat",56,"Horor",
+        HorrorBook b2 = new HorrorBook("doul2","murat",58,"Horor",
                 LocalDate.of(2021,2,4),LocalDate.now(),"Killer",6);
     manager.addBook(b1);
         manager.addBook(b2);
     DetectiveBook d1 = new DetectiveBook("crayz","murat",678,"Detective"
     ,LocalDate.of(2024,5,6),LocalDate.now(),7,"soul");
-        DetectiveBook d2 = new DetectiveBook("crayz","murat",678,"Detective"
+        DetectiveBook d2 = new DetectiveBook("crayz2","murat",679,"Detective"
                 ,LocalDate.of(2024,5,6),LocalDate.now(),7,"soul");
     manager.addBook(d1);
         manager.addBook(d2);
